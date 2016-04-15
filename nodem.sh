5c7777f3d41d63e453011beb13b5ee2974d13468
Marco Túlio de Ávila Santos <marco.santos@aker.com.br>
2016-04-15 16:13:43 -0300

    [new] available command and some refactorings
    Changes:
     - available command added
     - list_versions renamed to list_installed_versions
     - some DRY small, simple and helpful function
    
    Relevant:
     - versions directory changed to only number: v4.4.2 to 4.4.2

--- nodem.sh
+++ nodem.sh
@@ -17,24 +17,27 @@ function main {
         remove)
             remove_version $command;;
         list)
+            list_installed_versions;;
+        available)
             list_versions;;
         *)
             show_help;;
     esac
 }
 
+#
+# Installs the version <version> specified
+#
 function install {
-    command=$1
-    exit_if_empty "$command" "install"
+    version=$1
+    exit_if_empty "$version" "version"
 
-    # Parse version
-    local version=$(extract_version_from $command)
-    local targz="node-${version}-linux-x64.tar.gz"
+    local targz="node-v${version}-linux-x64.tar.gz"
 
     # Download targz to folder $version
     if [ ! -f ~/.nodem/tmp/$targz ]; then
-        warning "Downloading node-${version} files"
-        wget https://nodejs.org/dist/${version}/node-${version}-linux-x64.tar.gz -P $TMP_DIR
+        yellow "Downloading node-v${version} files"
+        wget https://nodejs.org/dist/v${version}/node-v${version}-linux-x64.tar.gz -P $TMP_DIR
         echo "Download successfully completed"
         echo
     fi
@@ -54,42 +57,44 @@ function install {
     # clean tmp dir
     rm -rf $TMP_DIR
     use_version $version
-    success "Node Version $version successfully installed"
+    green "Node Version $version successfully installed"
 }
 
 function use_version {
     local version=$1
     local option=$2
     if [[ -z "$version" ]]; then
-        warning "No version informed. Type: 'nodem use <version>'"
-        list_versions
+        yellow "No version informed. Type: 'nodem use <version>'"
+        list_installed_versions
         exit 1
     fi
     must_have_version $version
-    warning "Creating symbolic links. This requires root access."
+    yellow "Creating symbolic links. This requires root access."
     if [[ ! -z $option ]]; then
         sudo ln -sf $VERSIONS_DIR/$version/bin/npm /usr/local/bin/npm
-        success "Using npm version "`npm -v`
+        green "Using npm version "`npm -v`
     fi
-    sudo ln -sf $VERSIONS_DIR/$version/bin/node /usr/local/bin/node-$version
-    sudo ln -sf /usr/local/bin/node-$version /usr/local/bin/node
-    success "Default version changed to $version"
+    sudo ln -sf $VERSIONS_DIR/$version/bin/node /usr/local/bin/node-v$version
+    sudo ln -sf /usr/local/bin/node-v$version /usr/local/bin/node
+    green "Default version changed to $version"
     export PATH=$PREFIX/bin:$PATH
 }
 
 function must_have_version {
     local version=$1
     if [[ ! -d $VERSIONS_DIR/$version ]]; then
-        warning "Version $version not installed."
-        list_versions
+        yellow "Version $version not installed."
+        list_installed_versions
         exit 1
     fi
 }
 
-# Lista versões instaladas
-function list_versions {
+#
+# List installed versions
+#
+function list_installed_versions {
     if [[ ! -d $VERSIONS_DIR/$version ]]; then
-        warning "No version installed yet"
+        yellow "No version installed yet"
         echo "Use nodem install <version> to install some version"
         exit 0
     fi
@@ -104,20 +109,55 @@ function list_versions {
     echo "Type 'nodem remove <version>' to remove the specified version."
 }
 
+#
+# List available versions from dist dir
+#
+function list_versions {
+    echo "listing available versions"
+    dist_versions=`wget -O- https://nodejs.org/dist/ 2> /dev/null | egrep "</a>" \
+        | egrep -o '[0-9]+\.[0-9]+\.[0-9]+'\
+        | egrep -v '^0\.[0-7]\.'\
+        | egrep -v '^0\.8\.[0-5]$'\
+        | sort -u -k 1,1n -k 2,2n -k 3,3n -t .`
+    for version in $dist_versions; do
+        if is_installed $version; then
+            if is_current $version; then
+                list_selected `bold_green $version`
+            else
+                list `bold $version`
+            fi
+        else
+            list $version
+        fi
+    done
+}
+
+function list {
+    echo "    $1"
+}
+function list_selected {
+    echo "  o $1"
+}
+
+#
+# Removes the specified version
+#
 function remove_version {
     local version=$1
     exit_if_empty $version '<version>'
     must_have_version $version
     if [[ -d $VERSIONS_DIR/$version ]]; then
         rm -rf $VERSIONS_DIR/$version
-        success "Version $version removed"
+        green "Version $version removed"
     fi
-    if [[ -f "/usr/local/bin/node-$version" ]]; then
-        rm -rf /usr/local/bin/node-$version
+    if [[ -f "/usr/local/bin/node-v$version" ]]; then
+        rm -rf /usr/local/bin/node-v$version
     fi
 }
 
+#
 # Shows help command
+#
 function show_help {
     bold "Usage: "
     echo "    nodem <command> <version> <option>"
@@ -126,26 +166,45 @@ function show_help {
     echo "  - install <version>     Installs the node <version>."
     echo "  - use <version> <--npm> Creates symbolic links for the <version> installed."
     echo "  - remove <version>      Removes the node installed <version>."
+    echo "  - available             Lists all available node versions in https://nodejs.org/dist."
     echo "  - list                  Lists all installed node versions."
     echo "  - help                  Shows this help."
 }
 
-# Escreve a versao em negrito se for a versao em uso
+#
+# Echoes string in bold if it is the current node version
+#
 function echo_bold_if_current {
     local version=$1
     local current_version=`node -v 2> /dev/null`
-    if [[ $version == $current_version ]]; then
-        bold "    \e[1m$version\e[21m"
+    if [[ $version == ${current_version//v} ]]; then
+        bold "    $version"
     else
         echo "    $version"
     fi
 }
 
-function bold {
-    echo -e "\e[1m$1\e[21m"
+function is_current {
+    local version=$1
+    local current_version=`node -v 2> /dev/null`
+    if [[ $version == ${current_version//v} ]]; then
+        return 0
+    else
+        return 1
+    fi
 }
 
+function is_installed {
+    if [[ -d $VERSIONS_DIR/$1 ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+#
 # Exit script if param is empty
+#
 function exit_if_empty {
     if [[ -z $1 ]]; then
         echo "Missing argument <$2>"
@@ -153,15 +212,20 @@ function exit_if_empty {
     fi
 }
 
-function extract_version_from {
-    echo $1 | sed 's/.*dist\/\(v.*\)\/.*/\1/'
+function bold {
+    echo -e "\e[1m$1\e[21m"
 }
-
-function success {
+function green {
     echo -e "\e[32m${1}\e[0m"
 }
-function warning {
+function bold_green {
+    echo -e "\e[1;32m${1}\e[0m"
+}
+function yellow {
     echo -e "\e[33m${1}\e[0m"
+}
+function bold_yellow {
+    echo -e "\e[1;33m${1}\e[0m"
 }
 
 main "$@"
