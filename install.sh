f0c131e1414be7797d26d5ad81cd18353310c18f
Marco Túlio de Avila Santos <marcotulio.avila@gmail.com>
2019-01-14 16:31:35 -0200

    Fix filename on installation

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo wget https://raw.githubusercontent.com/marcoT89/nodem/master/nodem.sh -P /usr/local/bin/
+sudo wget -O /usr/local/bin/nodem https://raw.githubusercontent.com/marcoT89/nodem/master/nodem.sh
 sudo chmod +x /usr/local/bin/nodem
 
 echo 'export PATH=$HOME/.nodem/bin:$PATH # Add nodem to PATH for scripting' >> ~/.profile
